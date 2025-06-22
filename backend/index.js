5009dd3a17eee54d811c4c99e22e1f6246ee855b
mohd-mustufa <mohammedmustufa02@gmail.com>
2025-06-22 23:37:32 +0530

    Add cloudinary and express-fileupload to upload images to cloud

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import fileUpload from "express-fileupload";
+import { v2 as cloudinary } from "cloudinary";
 
-import courseRoute from './routes/course.route.js'
+import courseRoute from "./routes/course.route.js";
 
 const app = express();
 dotenv.config();
 
-const port = process.env.PORT || 3000;
-const DB_URI = process.env.MONGO_URI;
+// Middleware
+app.use(express.json());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
+// port
+const port = process.env.PORT || 3000;
+
+// Connect to DB
+const DB_URI = process.env.MONGO_URI;
 try {
   await mongoose.connect(DB_URI);
   console.log("Connected to MongoDB");
@@ -17,9 +30,15 @@ try {
   console.log(error);
 }
 
-// Defining Routes
-app.use('/api/v1/course', courseRoute)
+// Cloudinary configs
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
+// Defining Routes
+app.use("/api/v1/course", courseRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
